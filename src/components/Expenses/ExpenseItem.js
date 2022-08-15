1e7aa3861fa4a746e82ded33732b92fc306ea4f1
Carlos Calderon <cdcalderon@gmail.com>
2022-08-14 23:13:29 -0500

    add styles to expense item component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 export const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+  return (
+    <div className="expense-item">
+      <div className="expense-item__description">Title</div>
+    </div>
+  );
 };
